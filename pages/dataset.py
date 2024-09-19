8d833d261a9cc6def48ad640647545a9a6c5e88f
Monolana <naufalx26@gmail.com>
2024-09-19 10:22:31 +0700

    push the pages for streamlitApp

---
+++ pages/dataset.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.read_csv('processed.csv')
+st.dataframe(df)
+
+# CSS for styling the Streamlit app
+page_bg_color = """
+<style>
+[data-testid="stHeader"]{
+background-color: #10101C
+}
+
+[data-testid="stAppViewContainer"]{
+background-color: #10101C
+}
+</style>
+"""
+st.markdown(page_bg_color, unsafe_allow_html=True)
